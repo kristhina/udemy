ef8e48cd761d9a79b15472e69a8aa9aba8521577
Krystyna <krystyna.m.piatkowska@gmail.com>
2020-03-24 10:12:41 +0100

    section 6 lab 79

---
+++ Python Mobilo/section6_lab79_bez_klas.py
@@ -0,0 +1,32 @@
+cake_01_taste = 'vanilia'
+cake_01_glaze = 'chocolade'
+cake_01_text = 'Happy Brithday'
+cake_01_weight = 0.7
+
+cake_02_taste = 'tee'
+cake_02_glaze = 'lemon'
+cake_02_text = 'Happy Python Coding'
+cake_02_weight = 1.3
+
+
+def show_cake_info(taste, glaze, text, weight):
+    print('{} cake with {} glaze with text "{}" of {} kg'.format(
+        taste, glaze, text, weight))
+
+
+show_cake_info(cake_01_taste, cake_01_glaze, cake_01_text, cake_01_weight)
+show_cake_info(cake_02_taste, cake_02_glaze, cake_02_text, cake_02_weight)
+
+
+cake_01 = {'taste': 'vanilia', 'glaze': 'chocolade', 'text': 'Happy Birthday', 'weight': 0.7}
+cake_02 = {'taste': 'tee', 'glaze': 'lemon', 'text': 'Happy Python Coding', 'weight': 1.3}
+
+
+def show_cake_info_dict(cake):
+    print(cake['taste'], cake['glaze'], cake['text'], cake['weight'])
+
+
+list_of_cakes = [cake_01, cake_02]
+
+for cake in list_of_cakes:
+    show_cake_info_dict(cake)
