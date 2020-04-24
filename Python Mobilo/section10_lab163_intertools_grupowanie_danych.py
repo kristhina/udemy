97685ebf8f3a8783d919bb89f3f4e2aa956ff90e
Krystyna <krystyna.m.piatkowska@gmail.com>
2020-04-24 13:52:06 +0200

    section 10 lab 163

---
+++ Python Mobilo/section10_lab163_intertools_grupowanie_danych.py
@@ -0,0 +1,21 @@
+import os
+import itertools
+
+
+def scantree(path):
+    for entry in os.scandir(path):
+        if entry.is_dir():
+            yield entry
+            yield from scantree(entry.path)
+        else:
+            yield entry
+
+
+listing = scantree('/home/krysia/kursy/Python Mobilo/')
+for l in listing:
+    print('DIR ' if l.is_dir() else 'FILE', l.path)
+
+listing = sorted(listing, key=lambda e: e.is_dir())
+
+for is_dir, elements in itertools.groupby(listing, key=lambda e: e.is_dir()):
+    print('DIR ' if is_dir else 'FILE', len(list(elements)))
